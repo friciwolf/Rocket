0776e5f7b34c1b57dfba05481f0fe0a663f449e0
friciwolf <friciwolf@gmail.com>
2021-04-13 15:47:18 +0200

    Fine-tuned installer for Debian

--- installer/install.sh
+++ installer/install.sh
@@ -3,7 +3,8 @@
 mkdir -p ~/.local/share/icons/hicolor/scalable
 cp rocket.svg ~/.local/share/icons/hicolor/scalable/
 chmod +x rocket
-cp rocket ~/.local/bin
+mkdir -p ~/.local/bin/
+cp rocket ~/.local/bin/rocket
 chmod +x rocket.desktop
 cp rocket.desktop ~/.local/share/applications/
 mkdir ~/.config/rocket
