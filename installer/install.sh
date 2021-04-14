5dd7d40bd9bd103ba72d6f84f21e5131a3518293
grtcdr <ba.tahaaziz@gmail.com>
2021-04-14 22:27:13 +0100

    ✨ Add a warning to Rocket's installer

--- installer/install.sh
+++ installer/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+COLOR_RESET="$(tput sgr0)"
+COLOR_YELLOW="$(tput setaf 3)"
+
 mkdir -p ~/.local/share/icons/hicolor/scalable
 cp rocket.svg ~/.local/share/icons/hicolor/scalable/
 chmod +x rocket
@@ -9,3 +12,6 @@ chmod +x rocket.desktop
 cp rocket.desktop ~/.local/share/applications/
 mkdir ~/.config/rocket
 cp wallpaper.jpeg ~/.config/rocket
+
+echo "Rocket is installed now."
+echo "${COLOR_YELLOW}warning${COLOR_RESET}: be sure to add '$HOME/.local/bin' to your PATH to be able to run Rocket."
