ba56cd2c0dbcebaaca0c3daa66a0ccc238fdef15
Mate Farkas <friciwolf@gmail.com>
2021-09-21 16:01:18 +0200

    Added some comments in main.cpp

--- Rocket/main.cpp
+++ Rocket/main.cpp
@@ -18,19 +18,23 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QString name = a.applicationName().toLower();
+
+    // Create .config folder
     if (!QDir(QDir::homePath()+"/.config/"+name).exists())
-    {
         QDir().mkdir(QDir::homePath()+"/.config/"+name);
-    }
+
+    // Check if another instance of Rocket is running
     QLockFile mainlockFile(QDir::homePath()+"/.config/"+name+"/."+name+"main.lock");
     if (!mainlockFile.tryLock(100))
     {
+        // Kill Rocket if already running and exit
         QProcess p;
         p.start("pkill rocket");
         p.waitForFinished(1000);
         return 1;
     }
 
+    // Loading settings and show main window
     KConfig styleconfig(QDir::homePath()+"/.config/"+name+"/"+name+"style",KConfig::OpenFlag::SimpleConfig);
     KConfig appgridconfig(QDir::homePath()+"/.config/"+name+"/"+name+"appgrid",KConfig::OpenFlag::SimpleConfig);
 
